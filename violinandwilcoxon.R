fb861cf6114b5e42c6a854c7427d22c10739c2df
mbamg4 <133869792+mbamg4@users.noreply.github.com>
2023-05-18 14:12:23 +0100

    Update violinandwilcoxon.R

--- violinandwilcoxon.R
+++ violinandwilcoxon.R
@@ -7,25 +7,57 @@ library(dplyr)
 library(tidyr)
 library(viridis)
 library(readxl)
-table <- read_excel("NOTTS_NT001_ONT.xlsx")
 
-library(plyr)
-p_meds <- ddply(table, .(threshold), summarise, med = median(precision))
-ggplot(table, aes(x=threshold, y=precision, fill=threshold)) + 
-  geom_violin(trim=FALSE) + geom_boxplot(width=0.1) + theme_classic() + ylim(0,1) + geom_text(data = p_meds, aes(x = threshold, y = med, label = med), size = 2, vjust = -10) 
-dave <- ggplot(table, aes(x=threshold, y=sensitivity, fill=threshold)) + 
-  geom_violin(trim=FALSE) + geom_boxplot(width=0.1) + theme_classic() + ylim(0,1) + geom_text(data = p_meds, aes(x = threshold, y = med, label = med), size = 2, hjust = -0.5, vjust = -4) 
-
-library(tidyverse)
-library(ggpubr)
-library(rstatix)
-ggplot_build(dave)$data
+# Read the table from Excel file
+ILLtable <- read_excel("NOTTS_NT001_ILL.xlsx")
+ONTtable <- read_excel("NOTTS_NT001_ONT.xlsx")
 
 
-#friedman.test(sensitivity ~ sample | Run, data=as.matrix(table))
+# Create a violin plot and boxplot for precision
+p_meds <- ONTtable %>%
+  group_by(threshold) %>%
+  summarise(med = median(precision))
 
-ONTtable <- table %>% filter(threshold == "VF 0.01, MINCOV 1, MIN ALT READS 1, NO STRAND BIAS")
-ILLtable <- table %>% filter(threshold == "VF 0.03, MINCOV 8, MIN ALT READS 6, STRAND BIAS")
+precision_plot <- ggplot(ONTtable, aes(x = threshold, y = precision, fill = threshold)) +
+  geom_violin(trim = FALSE) +
+  geom_boxplot(width = 0.1) +
+  theme_classic() +
+  ylim(0, 1) +
+  geom_text(
+    data = p_meds,
+    aes(x = threshold, y = med, label = med),
+    size = 2,
+    vjust = -10
+  )
+
+# Save precision plot as PNG
+ggsave("precision_plot.png", plot = precision_plot, width = 6, height = 4)
+precision_plot
+# Create a violin plot and boxplot for sensitivity
+sensitivity_plot <- ggplot(ONTtable, aes(x = threshold, y = sensitivity, fill = threshold)) +
+  geom_violin(trim = FALSE) +
+  geom_boxplot(width = 0.1) +
+  theme_classic() +
+  ylim(0, 1) +
+  geom_text(
+    data = p_meds,
+    aes(x = threshold, y = med, label = med),
+    size = 2,
+    hjust = -0.5,
+    vjust = -4
+  )
+
+# Save sensitivity plot as PNG
+ggsave("sensitivity_plot.png", plot = sensitivity_plot, width = 6, height = 4)
+
+# Filter the table for specific threshold values
+
+ONTtable <- ONTtable %>% filter(threshold == "VF 0.01, MINCOV 1, MIN ALT READS 1, NO STRAND BIAS")
+ILLtable <- ILLtable %>% filter(threshold == "VF 0.01, MINCOV 1, MIN ALT READS 1, NO STRAND BIAS")
 bothtable <- rbind(ONTtable, ILLtable)
-wilcox.test(ONTtable$sensitivity, ILLtable$sensitivity, paired=TRUE, exact=TRUE) 
 
+# Perform Wilcoxon signed-rank test for sensitivity
+wilcox_result <- wilcox.test(ONTtable$sensitivity, ILLtable$sensitivity, paired = TRUE, exact = TRUE)
+
+# Print the test result
+print(wilcox_result)
